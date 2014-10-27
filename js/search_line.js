8e67ec3fccf4fdd6b2dd4986a202e4b7d985bcd6
Paul Meinshausen <meinshap@gmail.com>
2014-10-27 13:40:43 +0530

    update data

--- js/search_line.js
+++ js/search_line.js
@@ -1,5 +1,16 @@
 
 var search_list = [];
+var datearray = [];
+
+var tooltip = d3.select("div#line_chart")
+    .append("div")
+    .attr("class", "remove")
+    .style("position", "absolute")
+    .style("z-index", "20")
+    .style("visibility", "hidden")
+    .style("top", "500px")
+    .style("left", "500px")
+    .style("font-size", "16px");
 
 function updateData(frm){
   search_list.push(frm.toLowerCase());
@@ -212,7 +223,37 @@ function createChart(frm){
     termsLines
       .attr('d', function(d){ return line(d.values); })
       .style('stroke', function(d){ return line_color(d.term); })
-      .on('mouseover', function(d){ console.log(d.term, d.values); });
+      .on("mousemove", function(d, i) {
+        mousex = d3.mouse(this);
+        mousex = mousex[0];
+        var invertedx = x.invert(mousex);
+        //invertedx = invertedx.getMonth() + invertedx.getDate();
+        var selected = (d.values);
+        for (var k = 0; k < selected.length; k++) {
+          datearray[k] = selected[k].date
+          datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
+        }
+
+        mousedate = datearray.indexOf(Math.round(invertedx));
+        pro = d.values[mousedate].count;
+        tip_year = d.values[mousedate].year;
+
+        d3.select(this)
+        .classed("hover", true)
+        .attr("stroke", "#000")
+        .attr("stroke-width", "0.5px"), 
+        tooltip.html( "<p>" + d.term + "|" + tip._year + pro + "</p>" ).style("visibility", "visible");
+        
+      })
+      .on("mouseout", function(d, i) {
+       svg.selectAll(".layer")
+        .transition()
+        .duration(250)
+        .attr("opacity", "1");
+        d3.select(this)
+        .classed("hover", false)
+        .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.term + "|" + tip._year + pro + "</p>" ).style("visibility", "hidden");
+  })
 
   });
 }
