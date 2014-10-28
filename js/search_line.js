85dac3069b6f020664ff51e8a696fe1a221fed79
Paul Meinshausen <meinshap@gmail.com>
2014-10-28 17:46:42 +0530

    update data

--- js/search_line.js
+++ js/search_line.js
@@ -33,7 +33,7 @@ function showHeadlines(word){
     random_number = Math.floor((Math.random()*data.length) + 1)
     headline = data[random_number].headlineCol;
     headline_year = data[random_number].yearCol;
-    
+    console.log(typeof headline);
     return headline;
 
     /*
@@ -337,7 +337,7 @@ function createChart(frm){
     wordCircle
         .on('mouseover', function(d,i,j){
           the_headline = showHeadlines(used_data[j].term);
-          console.log(the_headline);
+          console.log(typeof the_headline);
             div.transition()
               .duration(200)
               .style('opacity', .9);
