f2f00e0fac4057ddc94975823d782d3cd1aa231a
Paul Meinshausen <meinshap@gmail.com>
2014-10-29 08:57:00 +0530

    update data

--- js/search_line.js
+++ js/search_line.js
@@ -1,3 +1,5 @@
+createChart('singapore');
+
 var margin = {top: 20, right: 20, bottom: 30, left: 140},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
