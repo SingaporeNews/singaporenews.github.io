340fb1d9d55794a66578b0c8e517cbb53cb51c70
Paul Meinshausen <meinshap@gmail.com>
2014-10-29 15:09:45 +0530

    update data

--- js/forceGraph.js
+++ js/forceGraph.js
@@ -76,21 +76,6 @@ d3.json("newsGraph.json", function(error, graph) {
       .links(graph.links)
       .start();
 
-    var fisheye = d3.fisheye.circular()
-          .radius(120);
-    forceSvg.on("mousemove", function() {
-          force.stop();
-          fisheye.focus(d3.mouse(this));
-          d3.selectAll("circle").each(function(d) { d.fisheye = fisheye(d); })
-              .attr("cx", function(d) { return d.fisheye.x; })
-              .attr("cy", function(d) { return d.fisheye.y; })
-              .attr("r", function(d) { return d.fisheye.z * 8; });
-          link.attr("x1", function(d) { return d.source.fisheye.x; })
-              .attr("y1", function(d) { return d.source.fisheye.y; })
-              .attr("x2", function(d) { return d.target.fisheye.x; })
-              .attr("y2", function(d) { return d.target.fisheye.y; });
-        });
-
     var graphMin = d3.min(graph.links, function(d){ return d.size; });
     var graphMax = d3.max(graph.links, function(d){ return d.size; });
 
