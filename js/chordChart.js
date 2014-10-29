54a5d74cfe9d64cdc74bfdf55ec52ac17353bbf6
Paul Meinshausen <meinshap@gmail.com>
2014-10-29 09:36:22 +0530

    update data

--- js/chordChart.js
+++ js/chordChart.js
@@ -47,7 +47,7 @@ d3.csv("cooccurrenceMatrixData.csv", function(data) {
 
   //selectedList = ['strike','workers','red','reds','talks','talk','jobs',''
 
-  data = data.filter(function(d){ return d.size > 200; });
+  data = data.filter(function(d){ return d.size > 300; });
   console.log(data);
   var words = {},
       array = [],
