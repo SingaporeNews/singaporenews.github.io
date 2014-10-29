8fd9f9e1503dbdbf07400670512aab3c6962712d
Paul Meinshausen <meinshap@gmail.com>
2014-10-29 13:04:58 +0530

    update data

--- js/forceGraph.js
+++ js/forceGraph.js
@@ -36,7 +36,7 @@ d3.json("newsGraph.json", function(error, graph) {
   new_graph['nodes'] = selectedData1;
   new_graph['links'] = selectedData3;
   
-  new_graph = new_graph.links.filter(function(d){ return typeof d.source != undefined });
+  new_graph = new_graph['links'].filter(function(d){ return typeof d.source != undefined });
   console.log(new_graph);
   drawGraph(new_graph);
 
