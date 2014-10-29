077daf1b403019c6284b42de893b7d1682745c3d
Paul Meinshausen <meinshap@gmail.com>
2014-10-29 12:01:25 +0530

    update data

--- js/forceGraph.js
+++ js/forceGraph.js
@@ -13,6 +13,7 @@ var forceSvg = d3.select("div#chord_chart").append("svg")
     .attr("height", height);
 
 d3.json("newsGraph.json", function(error, graph) {
+  console.log(graph);
   selectedList = [0,10,2,3,4,50,60,75,18];
   selectedData1 = $.map(graph.nodes, function(element){
       return ($.inArray(element.id,selectedList)>-1?element:null);
