d83e1e96f1ac4f3f74b6dfb65924507124c7e1cd
Paul Meinshausen <meinshap@gmail.com>
2014-10-30 14:27:12 +0800

    resize window

--- js/streamgraph.js
+++ js/streamgraph.js
@@ -77,7 +77,7 @@ var w = window,
     d = document,
     e = d.documentElement,
     g = d.getElementsByTagName('div#chart')[0],
-    width = w.innerWidth || e.clientWidth || g.clientWidth,
+    width = g.clientWidth, //w.innerWidth || e.clientWidth || g.clientWidth,
     height = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
