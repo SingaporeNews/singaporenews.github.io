ac5bdc84e124f49c44a863b8374fc0444cd3925c
Paul Meinshausen <meinshap@gmail.com>
2014-10-30 15:47:25 +0800

    resize window

--- js/forceGraph.js
+++ js/forceGraph.js
@@ -186,7 +186,7 @@ function drawGraph(selectedList){
                   .duration(500)
                   .style('opacity', 1);
                 div.html(d.name)
-                  .style('left', '50px')
+                  .style('left', '0px')
                   .style('top', '10px');
                   //.style('left', (d3.event.pageX) + 10 + "px")
                   //.style('top', (d3.event.pageY) + 2 + "px")
